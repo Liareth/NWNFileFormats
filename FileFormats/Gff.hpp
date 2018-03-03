60503fc6b85eb535634eeba2f69c674172440873
Liareth <liarethnwn@gmail.com>
2018-03-03 20:05:35 +0000

    Updated comments in Gff.hpp.

--- FileFormats/Gff.hpp
+++ FileFormats/Gff.hpp
@@ -1,15 +1,15 @@
 #pragma once
 
 // This file provides read-only access to GFF data.
-// In the FileFormats::Raw namespace is located Gff, which wraps the raw data structure.
-// In the FileFormats::Friendly namespace is located Gff, which exposes a much more user friendly structure.
+// In the FileFormats::Gff::Raw namespace is located Gff, which wraps the raw data structure.
+// In the FileFormats::Gff::Friendly namespace is located Gff, which exposes a much more user friendly structure.
 //
 // How to use:
 //
 // Step 1: Load your GFF file into memory.
-// Step 2: Construct a Gff as such: FileFormats::Raw::Gff::ReadFromBytes(bytes);
+// Step 2: Construct a Gff as such: FileFormats::Gff::Raw::Gff::ReadFromBytes(bytes);
 // - You can browse the loaded field format and extract fields using the ConstructX functions.
-// Step 3: If user friendly access to fields in desired, construct a Gff from FileFormats::Friendly::Gff(rawGff).
+// Step 3: If user friendly access to fields is desired, construct a Gff from FileFormats::Gff::Friendly::Gff(rawGff).
 // - You can access the top level struct with GetTopLevelStruct().
 // - You can access fields with GetTopLevelStruct().ReadField<Type_CExoString>("FIELD_NAME").
 //
